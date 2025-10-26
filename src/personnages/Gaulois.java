5aa9392c1878af26d3dcddd2e41a4c9b301ca7c6
Me56de <mederic.mognat@utoulouse.fr>
2025-10-26 18:54:09 +0100

    Fin du premier TP de GL

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,23 @@
+package personnages;
+
+public class Gaulois {
+	private int force;
+	private String nom;
+	
+	public Gaulois(int force, String nom) {
+		this.force = force;
+		this.nom = nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+}
