6b6379cbfa9ea16fca99c7ac02221841561e3713
Me56de <mederic.mognat@utoulouse.fr>
2025-10-27 18:24:42 +0100

    invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,33 +3,47 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	
+
+	private boolean isInvariantVerified() {
+		return force >= 0;
+	}
+
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified();
 	}
 
 	public String getNom() {
 		return nom;
 	}
-	
-	
+
 	public int getForce() {
 		return force;
 	}
 
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "\"" + texte + "\"");
-		
+
 	}
 
 	private String prendreParole() {
 		return "Le Romain " + nom + " : ";
 	}
-	
+
 	public void recevoirCoup(int forceCoup) {
-		force = force-forceCoup;
-		if (force<1) parler("J'abandonne !");
-		else parler("Aie");	
+		assert forceCoup>=0;
+		int force_initial=force;
+		force = force - forceCoup;
+		if (force < 1)
+			parler("J'abandonne !");
+		else
+			parler("Aie");
+		assert force_initial < force;
+		assert isInvariantVerified();
+	}
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus",6);
 	}
 }
